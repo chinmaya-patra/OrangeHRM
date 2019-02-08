71e4067b4d80b9ccf012efb3268650e2faed9549
chinmaya-patra <42782560+chinmaya-patra@users.noreply.github.com>
2019-02-08 19:50:31 +0530

    login testcase scripts added

--- src/test/java/com/automation/pages/LoginPage.java
+++ src/test/java/com/automation/pages/LoginPage.java
@@ -1,5 +1,30 @@
 package com.automation.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage {
+	
+	WebDriver driver;
+	
+	
+	public LoginPage(WebDriver ldriver) {
+		
+		this.driver=ldriver;
+	}
+	
+	@FindBy(name="txtUsername") WebElement userNameBox;
+	@FindBy(name="txtPassword") WebElement passwordBox;
+	@FindBy(name="Submit") WebElement loginBtn;
+	
+	
+	
+	public void loginToCrm(String username,String password) {
+		userNameBox.sendKeys(username);
+		passwordBox.sendKeys(password);
+		loginBtn.click();
+		
+	}
 
 }
--- src/test/java/com/automation/testCases/LoginTest.java
+++ src/test/java/com/automation/testCases/LoginTest.java
@@ -1,8 +1,10 @@
 package com.automation.testCases;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
+import com.automation.pages.LoginPage;
 import com.automation.utilities.BrowserFactory;
 
 public class LoginTest {
@@ -17,7 +19,14 @@ public void loginApp() {
 	
 	
 driver=	BrowserFactory.startApplication(driver, "chrome", "http://127.0.0.1/orangehrm/symfony/web/index.php/auth/login");
-    System.out.println(driver.getTitle());
+LoginPage loginpage=PageFactory.initElements(driver, LoginPage.class);
+loginpage.loginToCrm("chinmaya", "inspiron1545");
+
+
+
+System.out.println(driver.getTitle());
+
+BrowserFactory.quitBrowser(driver);
 
 }
 }
